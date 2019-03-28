83ec28175b58cf66da102087ed4ae7b5f6cddf7f
周顺 <yunzhong1075@126.com>
2019-03-28 15:21:47 +0800

    add code

---
+++ ZSTest/ZSTestObj.h
@@ -0,0 +1,19 @@
+//
+//  ZSTestObj.h
+//  ddd
+//
+//  Created by Shaun on 2019/3/28.
+//  Copyright © 2019 Shaun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZSTestObj : NSObject
+
++ (void)testFunc;
+
+@end
+
+NS_ASSUME_NONNULL_END
