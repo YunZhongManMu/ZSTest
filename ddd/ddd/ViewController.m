8ba39e07d7a71eadf395f7d2bcd73fda4ecfff94
周顺 <yunzhong1075@126.com>
2019-03-28 15:51:08 +0800

    add

---
+++ ddd/ddd/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  ddd
+//
+//  Created by Shaun on 2019/3/28.
+//  Copyright © 2019 Shaun. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "ZSTest/ZSTestObj.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    [ZSTestObj testFunc];
+}
+
+
+@end
